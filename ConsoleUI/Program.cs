ea0e875d69f08c5a3197819c25c59209b006e6d7
cagataysarioglu <sarioglucagatay@gmail.com>
2021-02-02 13:16:08 +0300

    The mechanism added into the ui.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
 
 namespace ConsoleUI
 {
@@ -6,6 +8,12 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
         }
     }
 }
