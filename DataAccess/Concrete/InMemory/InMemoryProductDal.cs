ba05cac80f168b31b464805a7652d50f1ce1bf99
cagataysarioglu <sarioglucagatay@gmail.com>
2021-02-02 12:44:20 +0300

    DataAccess layer completed.

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -50,6 +50,11 @@ namespace DataAccess.Concrete.InMemory
             return _products;
         }
 
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
         public void Update(Product product)
         {
             Product productToUpdate = _products.SingleOrDefault(p => product.ProductId == p.ProductId);
