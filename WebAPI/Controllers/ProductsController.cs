d7099e86bb6040c8812025876dbaf3e6b18334dc
Çağatay Sarıoğlu <sarioglucagatay@gmail.com>
2021-03-12 14:45:51 +0300

    A delay added for snappings.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Business.Concrete;
@@ -25,6 +26,8 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+            Thread.Sleep(5000);
+
             var result = _productService.GetAll();
             if (result.Success)
             {
