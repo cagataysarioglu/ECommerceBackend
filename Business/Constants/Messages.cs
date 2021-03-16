79fa7e2e6b55102e10a7bfaa9ba95ef7a15cb9a2
Çağatay Sarıoğlu <sarioglucagatay@gmail.com>
2021-03-16 11:32:21 +0300

    Constant messages are renewed.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,10 +8,10 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string Added = "Item added successfully.";
+        public static string Added = "Item is added successfully.";
         public static string Deleted = "Item that you selected is deleted.";
         public static string Updated = "Item that you selected is updated.";
-        public static string Listed = "Items that you requested is listed..";
+        public static string Listed = "Items that you requested are listed..";
         public static string NameInvalid = "Item name is invalid.";
         public static string MaintenanceTime = "Service is unavailable.";
         public static string ProductCountOfCategoryError = "Out of category count limit.";
